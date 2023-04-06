4c7e007308933f0acddeb1dce0c3e5c6581a104c
Kubra <Kubra@DESKTOP-J3V4CEL>
2023-04-06 15:45:15 +0300

    Add project files.

---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+
+        public void AddCategory(Category category)
+        {
+            c.Add(category);
+            c.SaveChanges();//Değişiklikleri kaydediyor
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            c.Remove(category);
+            c.SaveChanges();
+        }
+
+        public Category GetById(int id)
+        {
+            return c.Categories.Find(id);
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            return c.Categories.ToList();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            c.Update(category);//Entity framvework core ile beraber gelen method
+            c.SaveChanges();
+        }
+    }
+}
